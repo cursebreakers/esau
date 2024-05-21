e016d67f2d1fdb4ed21972176b86fcfb4bfc4ecd
Esau <cursebreakers.usa@gmail.com>
2024-05-21 09:57:11 -0400

    introduction

--- resources/hello.js
+++ resources/hello.js
@@ -3,10 +3,10 @@
 function hello(message) {
     // This function takes a "message" string 
     // Posts it as the page heading, and then logs it to the console. 
-    // Try it: hello("Check it out!")
+    // Try it: hello("It works!")
     const helloWorld = document.getElementById('blogTitle')
     helloWorld.innerHTML = message;
     console.log(message);
 }
 
-hello("Hello World!");
+hello("Hello, World!");
